7fd1ba3ffc1366526ac14b51a07b3a0ba3eb72a9
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-12-09 22:20:32 -0700

    [DAY 1] PART 2 completed

--- 2022/day_1/calorie_counter.rb
+++ 2022/day_1/calorie_counter.rb
@@ -17,16 +17,38 @@ def elf_with_most_calories
   puts most_calories
 end
 
+def top_3_with_most_calories
+  calories_by_elf = parse_calories_per_elf
+  calories_by_elf.each do |cals|
+    Elf.new(cals)
+  end
+  Elf.all.sort_by(&:calories).last(3)
+end
+
+def summed_top_3_calories
+  puts top_3_with_most_calories.map(&:calories).sum
+end
+
 # Struct where calories is an array of calorie elements in string form
 # the struct converts array elements to int, and sums them to list the total calories
 #
-Elf = Struct.new(:calories) do
+class Elf
+  attr_accessor :calories
+
+  # array of elf objects, which can be sorted by elf.calories
+  @@all = []
+
   def initialize(calories)
     self.calories = muddle(calories)
+    @@all << self
   end
 
   def muddle(array)
     array.grep(/\d+/, &:to_i).sum
+  end
+
+  def self.all
+    @@all
   end
 end
 
@@ -35,6 +57,8 @@ def parse_calories_per_elf(pathname = INPUT, delimiter = DELIMITER)
   lines.map! { |line| line.split(delimiter) }
 end
 
-elf_with_most_calories
+# PART ONE:
+# elf_with_most_calories
 
-# PART TWO: WE WANT TO RETURN THE TOP THREE with most_calories
+# PART TWO:
+summed_top_3_calories
